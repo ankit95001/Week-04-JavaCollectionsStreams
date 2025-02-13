f60f62966c43c1b1f68ed0452bf00bc19f8ec492
Ankit_kumar <ankit95001kumar@gmail.com>
2025-02-13 12:26:00 +0530

    [Ankit] add Test cases for Piped Streams - Inter-Thread Communication

---
+++ src/test/java/com/example/day03javastreams/pipedstreams/PipedStreamTest.java
@@ -0,0 +1,37 @@
+package com.example.day03javastreams.pipedstreams;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import java.io.*;
+
+class PipedStreamTest {
+    private PipedOutputStream pos;
+    private PipedInputStream pis;
+
+    @BeforeEach
+    void setup() throws IOException {
+        pos = new PipedOutputStream();
+        pis = new PipedInputStream(pos);
+    }
+
+    @AfterEach
+    void cleanup() throws IOException {
+        pos.close();
+        pis.close();
+    }
+
+    @Test
+    void testInterThreadCommunication() throws IOException, InterruptedException {
+        WriterThread writer = new WriterThread(pos);
+        ReaderThread reader = new ReaderThread(pis);
+
+        writer.start();
+        reader.start();
+
+        writer.join();
+        reader.join();
+
+        assertEquals("Hello from WriterThread!", reader.getReceivedData());
+    }
+}
+
