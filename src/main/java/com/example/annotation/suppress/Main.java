e18743153adcfd87ff2047e1064916ccb6556dfb
Ankit_kumar <ankit95001kumar@gmail.com>
2025-02-16 22:25:53 +0530

    [Ankit] added Exercise 3: Suppress Unchecked Warnings

---
+++ src/main/java/com/example/annotation/suppress/Main.java
@@ -0,0 +1,9 @@
+package com.example.annotation.suppress;
+
+public class Main {
+    public static void main(String[] args) {
+        UncheckedWarningExample example = new UncheckedWarningExample();
+        example.addElements();
+    }
+}
+
