276e18ea75ba5e0155fe38f6ffecb785c87652c1
Ankit_kumar <ankit95001kumar@gmail.com>
2025-02-16 22:50:56 +0530

    [Ankit] added Implement a Role-Based Access Control with @RoleAllowed

---
+++ src/main/java/com/example/annotation/advancedlevel/rolebasedaccess/RoleAllowed.java
@@ -0,0 +1,10 @@
+package com.example.annotation.advancedlevel.rolebasedaccess;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RoleAllowed {
+    String value();
+}
+
