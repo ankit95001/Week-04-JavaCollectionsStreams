4a34b99559357792525a48204b0af2a0060f32bf
Ankit_kumar <ankit95001kumar@gmail.com>
2025-02-16 22:20:27 +0530

    [Ankit] added Exercise 1: Use @Override Correctly

---
+++ src/main/java/com/example/annotation/override/Main.java
@@ -0,0 +1,9 @@
+package com.example.annotation.override;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        myDog.makeSound();
+    }
+}
+
